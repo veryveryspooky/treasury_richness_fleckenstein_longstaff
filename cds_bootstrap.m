15eae53d33c1bf6bcb2cb9886da22e7767068acc
Jayanth Indraganti <jayanth@gatech.edu>
2022-04-24 08:53:54 -0400

    fix interest rate clean python typo, get hazard rates for one date

--- cds_bootstrap.m
+++ cds_bootstrap.m
@@ -1,12 +1,15 @@
 cds_data = readtable('./cds_data_processed.csv');           
 interest_rates = readtable('./interest_rates_from_df.csv');
 %%
-settlement_date = '2014-09-18';
+start_date = datenum('2015-01-01');
+end_date = datenum('2022-03-31');
+for settlementdate = start_date:end_date
+
 sd_dn = datenum(settlement_date);
 r = interest_rates(interest_rates.Date == settlement_date, :);
 %%
-zero_rates = table2array(r(:, 16:end))'/100;
-zero_dates = datenum(table2array(r(:, 2:15)))';
+zero_rates = table2array(r(:, 17:end))'/100;
+zero_dates = datenum(table2array(r(:, 2:16)))';
 zero_data = [zero_dates zero_rates];
 %%
 mkt = cds_data(cds_data.SettlementDate == settlement_date, :);
@@ -16,5 +19,6 @@ mkt_data = [mkt_dates, mkt_rates];
 %%
 [ProbData,HazData] = cdsbootstrap(zero_data,mkt_data,sd_dn, 'ZeroCompounding', -1);
 
+end
 %%
-plot(zero_rates)
+plot(HazData(:,2))
